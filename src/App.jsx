73e484419765819e233df341cbf4ebf706efdbb5
Leon Chapman <lchapman9955@gmail.com>
2025-12-23 19:42:45 +0000

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/styles/GlobalStyles.js 	modified:   src/ui/Button.jsx 	new file:   src/ui/Input.jsx 	new file:   src/ui/Row.jsx 	new file:   src/ui/heading.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/heading";
+import Row from "./ui/Row";
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The wild oasis</Heading>
+            <Heading as="h2">Buttons which do things</Heading>
+            <Button>Check in</Button>
+            <Button variation="primary" size="medium">
+              Check out
+            </Button>
+          </Row>
+          <Row>
+            <Heading as="h3">Checkin/Checkout details</Heading>
+            <Input type="number" placeholder="Number of guests" />
+          </Row>
+        </Row>
+      </div>
+    </>
+  );
 }
